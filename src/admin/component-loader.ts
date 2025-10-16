7a02d004b3924e18d4a19274f15264335ce817d6
Mohamed Yasser <mohamed3laily@gmail.com>
2025-10-17 01:21:49 +0300

    fixed react component loading

--- src/admin/component-loader.ts
+++ src/admin/component-loader.ts
@@ -2,7 +2,7 @@ import { ComponentLoader } from 'adminjs';
 const componentLoader = new ComponentLoader();
 
 const Components = {
-  DashboardComponent: componentLoader.add('Dashboard', '../components/Dashboard.tsx'),
+  DashboardComponent: componentLoader.add('Dashboard', '../components/Dashboard'),
 };
 
 export { componentLoader, Components };
